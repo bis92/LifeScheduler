06756cf224369aaa5c7af201631e9fd972433d73
Bis92 <bis920309@gmail.com>
2021-04-17 20:37:31 +0900

    final status

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -1,3 +1,3 @@
 //SERVER ROUTES
-export const USER_SERVER = "http://13.125.217.93/api/users";
-export const SCHEDULE_SERVER = "http://13.125.217.93/api/schedules";
+export const USER_SERVER = "/api/users";
+export const SCHEDULE_SERVER = "/api/schedules";
